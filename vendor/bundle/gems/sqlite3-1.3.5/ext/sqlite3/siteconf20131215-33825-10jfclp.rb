8c04c8d0ff7accddc3a1b803f4a26887ff880027
jochenf36 <jochen.francois@gmail.com>
2013-12-15 16:47:47 +0100

    initials commit

---
+++ vendor/bundle/gems/sqlite3-1.3.5/ext/sqlite3/siteconf20131215-33825-10jfclp.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131215-33825-j6dhbd"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
---
+++ vendor/bundle/gems/sqlite3-1.3.8/ext/sqlite3/siteconf20131215-33371-1igdq1a.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131215-33371-1byn4k0"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
